5df9f031a118cf0584fc6bac9517daa4dfbbb87d
mehgokalp <mehgokalp98@gmail.com>
2016-04-16 15:33:48 +0300

    Relased

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="tr" itemscope itemtype="http://schema.org/Person">
+    <head>
+        <meta name="google-site-verification" content="L--R4Xp6RYRJcmJQjV9emLK5WKbmDaG4dNpgyQWHJnQ" />
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul">
+        <meta name="author" content="Mehmet Gökalp">
+        <title>Mehmet Gökalp Kişisel Web Sayfası, Ataşehir/İstanbul</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="alternate" href="http://mehmetgokalp.com.tr/" hreflang="tr"/>
+        <link rel="canonical" href="http://mehmetgokalp.com.tr/">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta http-equiv="content-language" content="tr">
+        <meta name="robots" content="index, nofollow">
+        <meta name="owner" content="Mehmet Gökalp" />
+        <meta name="copyright" content="(C) 2015" />
+        <link rel="icon" href="img/favicon_32.png?v=1.2">
+
+        <!-- For third-generation iPad with high-resolution Retina display: -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon_144.png">
+
+        <!-- For iPhone with high-resolution Retina display: -->
+
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon_114.png">
+
+        <!-- For first- and second-generation iPad: -->
+
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon_72.png">
+
+        <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+
+        <link rel="apple-touch-icon-precomposed" href="img/favicon_32.png">
+
+        <!-- Schema.org markup for Google+ -->
+        <meta itemprop="name" content="Webmaster Mehmet Gökalp">
+        <meta itemprop="description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul">
+        <meta itemprop="image" content="http://www.example.com/image.jpg">
+
+        <!-- Twitter Card data -->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@MehGokalp" />
+        <meta name="twitter:title" content="Mehmet Gökalp Kişisel Web Sayfası, Ataşehir/İstanbul" />
+        <meta name="twitter:description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul" />
+        <meta name="twitter:image" content="http://mehmetgokalp.com.tr/img/mehmetgokalp_logo_70x50.png" />
+
+        <!-- Open Graph data -->
+        <meta property="og:title" content="Webmaster Mehmet Gökalp" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:url" content="http://mehmetgokalp.com.tr/" />
+        <meta property="og:image" content="http://mehmetgokalp.com.tr/img/mehmetgokalp_logo_70x50.png" />
+        <meta property="og:description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul" />
+        <meta property="og:site_name" content="Webmaster Mehmet Gökalp" />
+
+        <style>
+            /* code for animated blinking cursor */
+            .typed-cursor{
+                opacity: 1;
+                font-weight: 100;
+                -webkit-animation: blink 0.7s infinite;
+                -moz-animation: blink 0.7s infinite;
+                -ms-animation: blink 0.7s infinite;
+                -o-animation: blink 0.7s infinite;
+                animation: blink 0.7s infinite;
+            }
+            @-keyframes blink{
+                0% { opacity:1; }
+                50% { opacity:0; }
+                100% { opacity:1; }
+            }
+            @-webkit-keyframes blink{
+                0% { opacity:1; }
+                50% { opacity:0; }
+                100% { opacity:1; }
+            }
+            @-moz-keyframes blink{
+                0% { opacity:1; }
+                50% { opacity:0; }
+                100% { opacity:1; }
+            }
+            @-ms-keyframes blink{
+                0% { opacity:1; }
+                50% { opacity:0; }
+                100% { opacity:1; }
+            }
+            @-o-keyframes blink{
+                0% { opacity:1; }
+                50% { opacity:0; }
+                100% { opacity:1; }
+            }
+        </style>
+    </head>
+    <body>
+        <section class="terminal">
+            <header class="terminal-header">
+                <div class="icons">
+                    <a class="icon red" href="javascript:;">
+                        <i class="fa fa-times"></i>
+                    </a>
+                    <a class="icon orange" href="javascript:;">
+                        <i class="fa fa-minus"></i>
+                    </a>
+                    <a class="icon green" href="javascript:;">
+                        <i class="fa fa-expand"></i>
+                    </a>
+                </div>
+
+                <img src="img/mehmetgokalp_logo.png" class="mg-logo" alt="Mehmet Gökalp Logo"> Mehmet Gökalp — -bash — 80 x 24
+            </header>
+            <article class="terminal-body">
+                <div id="messages" style="display: none">
+                    <p><code>Merhaba, ben <span itemprop="name">Mehmet Gökalp.</span></code> ^300
+                        <code>Öncelikle hoşgeldin. ^200 Sana biraz kendimden bahsedeyim.</code> ^200
+                        <code>18 yaşında, kendini programcılığa adamış bir <span itemprop="jobTitle">Webmaster</span>'ım.</code> ^200
+                        <code>Boş vakitlerimde kendimi, alanımda geliştirmeye çalışır, yeni teknolojiler öğrenmeye gayret gösteririm.</code> ^400
+                        <code>Bu işe masaüstü programları yazarak başladım akabinde web üzerinde çalışmalarıma devam ettim.</code> ^300
+                        <code>Şu ana kadar Php, Asp.NET, CSS(SASS), HTML, ActionScript, Mysql, Mongo, NodeJs, Javascript, JQuery, Ajax, C#, Phyton, Lua, C++, JSON, YML, XML gibi dilleri, teknolojileri OS X, UNIX(Ubuntu, FreeBSD) ve Windows platformları üzerinde kullandım. </code> ^300
+                        <code>Şuan aktif olarak kullandıklarım:</code>  ^100
+                        <code>PHP, CSS(SASS), HTML, Mysql, Mongo, NodeJs, Javascript, JQuery, Ajax, JSON, YML, XML, PHP framework'u olarak <a href="https://symfony.com/" rel="nofollow" target="_blank" itemprop="url">Symfony</a> ve OS X, Linux platformları</code> ^300
+                        <code>Peki ben ne yaparım?</code>  ^300
+                        <code>Her çaptaki Web projesinde yer alabilir, Freelance işleriniz için yardımcı olabilir, Profosyonel, hızlı botlar yazabilir ve daha pek çoğunu yapabilirim.</code>  ^500
+                        <code>Bana ulaşmak istersen: <a href="mailto:iletisim@mehmetgokalp.com.tr" itemprop="email">iletisim@mehmetgokalp.com.tr</a> adresine mail atmayı veya <a href="tel:05375776564" itemprop="telephone">0(537) 577-65-64</a> numaralı telefonu aramayı unutma!</code>  ^100
+                        <code>Vakit ayırdığın için teşekkürler. İyi günler! :)</code></p>
+                </div>
+                <span id="message" style="white-space: pre-line;"></span>
+            </article>
+        </section>
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src="js/typed.min.js"></script>
+        <script>
+            $(document).on('ready', function(){
+                var anim = false,
+                    isTyping = true;
+
+                $('#message').typed({
+                    stringsElement: $('#messages'),
+                    typeSpeed: 30,
+                    backDelay: 500,
+                    loop: false,
+                    contentType: 'html',
+                    loopCount: false,
+                    callback: function(){
+                        isTyping = false;
+                    }
+                });
+
+                setInterval(function(){
+                    if(!anim && isTyping){
+                        anim = true;
+                        $('.terminal-body').animate({ scrollTop: $('.terminal-body').prop("scrollHeight")}, 100, function(){
+                            anim = false;
+                        });
+                    }
+                }, 100);
+            });
+        </script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-75852991-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </body>
+</html>
