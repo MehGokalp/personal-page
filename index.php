b0e2019e00a377a0a30f4b81613ca8b8f441a1f3
Mehmet Gökalp <mehmet.gokalp@enuygun.com>
2018-08-26 14:12:29 +0300

    Typedjs updated, stop animation feature added and language changed to english

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
         <meta name="google-site-verification" content="L--R4Xp6RYRJcmJQjV9emLK5WKbmDaG4dNpgyQWHJnQ" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul">
+        <meta name="description" content="Mehmet Gökalp Personal Web Site">
         <meta name="author" content="Mehmet Gökalp">
-        <title>Mehmet Gökalp Kişisel Web Sayfası, Ataşehir/İstanbul</title>
+        <title>Mehmet Gökalp Personal Web Site</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="alternate" href="http://mehmetgokalp.com.tr/" hreflang="tr"/>
-        <link rel="canonical" href="http://mehmetgokalp.com.tr/">
+        <link rel="alternate" href="http://mehmetgokalp.com/" hreflang="tr"/>
+        <link rel="canonical" href="http://mehmetgokalp.com/">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="HandheldFriendly" content="True">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -36,22 +36,22 @@
         <link rel="apple-touch-icon-precomposed" href="img/favicon_32.png">
 
         <!-- Schema.org markup for Google+ -->
-        <meta itemprop="name" content="Webmaster Mehmet Gökalp">
-        <meta itemprop="description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul">
-        <meta itemprop="image" content="http://www.example.com/image.jpg">
+        <meta itemprop="name" content="Mehmet Gökalp">
+        <meta itemprop="description" content="Mehmet Gökalp Personal Web Site">
+        <meta itemprop="image" content="http://mehmetgokalp.com/img/mehmetgokalp_logo_70x50.png">
 
         <!-- Twitter Card data -->
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@MehGokalp" />
-        <meta name="twitter:title" content="Mehmet Gökalp Kişisel Web Sayfası, Ataşehir/İstanbul" />
-        <meta name="twitter:description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul" />
-        <meta name="twitter:image" content="http://mehmetgokalp.com.tr/img/mehmetgokalp_logo_70x50.png" />
+        <meta name="twitter:title" content="Mehmet Gökalp Personal Web Site" />
+        <meta name="twitter:description" content="Mehmet Gökalp Personal Web Site" />
+        <meta name="twitter:image" content="http://mehmetgokalp.com/img/mehmetgokalp_logo_70x50.png" />
 
         <!-- Open Graph data -->
-        <meta property="og:title" content="Webmaster Mehmet Gökalp" />
+        <meta property="og:title" content="Mehmet Gökalp Personal Web Site" />
         <meta property="og:type" content="profile" />
-        <meta property="og:url" content="http://mehmetgokalp.com.tr/" />
-        <meta property="og:image" content="http://mehmetgokalp.com.tr/img/mehmetgokalp_logo_70x50.png" />
+        <meta property="og:url" content="http://mehmetgokalp.com/" />
+        <meta property="og:image" content="http://mehmetgokalp.com/img/mehmetgokalp_logo_70x50.png" />
         <meta property="og:description" content="Webmaster Mehmet Gökalp'in Kişisel Web Sayfası, Ataşehir/İstanbul" />
         <meta property="og:site_name" content="Webmaster Mehmet Gökalp" />
 
@@ -97,13 +97,13 @@
         <section class="terminal">
             <header class="terminal-header">
                 <div class="icons">
-                    <a class="icon red" href="javascript:;">
+                    <a class="icon red" href="javascript:">
                         <i class="fa fa-times"></i>
                     </a>
-                    <a class="icon orange" href="javascript:;">
+                    <a class="icon orange" href="javascript:">
                         <i class="fa fa-minus"></i>
                     </a>
-                    <a class="icon green" href="javascript:;">
+                    <a class="icon green" href="javascript:">
                         <i class="fa fa-expand"></i>
                     </a>
                 </div>
@@ -111,51 +111,79 @@
                 <img src="img/mehmetgokalp_logo.png" class="mg-logo" alt="Mehmet Gökalp Logo"> Mehmet Gökalp — -bash — 80 x 24
             </header>
             <article class="terminal-body">
-                <div id="messages" style="display: none">
-                    <p><code>Merhaba, ben <span itemprop="name">Mehmet Gökalp.</span></code> ^300
-                        <code>Öncelikle hoşgeldin. ^200 Sana biraz kendimden bahsedeyim.</code> ^200
-                        <code>18 yaşında, kendini programcılığa adamış bir <span itemprop="jobTitle">Webmaster</span>'ım.</code> ^200
-                        <code>Boş vakitlerimde kendimi, alanımda geliştirmeye çalışır, yeni teknolojiler öğrenmeye gayret gösteririm.</code> ^400
-                        <code>Bu işe masaüstü programları yazarak başladım akabinde web üzerinde çalışmalarıma devam ettim.</code> ^300
-                        <code>Şu ana kadar Php, Asp.NET, CSS(SASS), HTML, ActionScript, Mysql, Mongo, NodeJs, Javascript, JQuery, Ajax, C#, Phyton, Lua, C++, JSON, YML, XML gibi dilleri, teknolojileri OS X, UNIX(Ubuntu, FreeBSD) ve Windows platformları üzerinde kullandım. </code> ^300
-                        <code>Şuan aktif olarak kullandıklarım:</code>  ^100
-                        <code>PHP, CSS(SASS), HTML, Mysql, Mongo, NodeJs, Javascript, JQuery, Ajax, JSON, YML, XML, PHP framework'u olarak <a href="https://symfony.com/" rel="nofollow" target="_blank" itemprop="url">Symfony</a> ve OS X, Linux platformları</code> ^300
-                        <code>Peki ben ne yaparım?</code>  ^300
-                        <code>Her çaptaki Web projesinde yer alabilir, Freelance işleriniz için yardımcı olabilir, Profosyonel, hızlı botlar yazabilir ve daha pek çoğunu yapabilirim.</code>  ^500
-                        <code>Bana ulaşmak istersen: <a href="mailto:iletisim@mehmetgokalp.com.tr" itemprop="email">iletisim@mehmetgokalp.com.tr</a> adresine mail atmayı veya <a href="tel:05375776564" itemprop="telephone">0(537) 577-65-64</a> numaralı telefonu aramayı unutma!</code>  ^100
-                        <code>Vakit ayırdığın için teşekkürler. İyi günler! :)</code></p>
+                <div id="messages">
+                    <p>
+                        <code>Hello, my name is Mehmet Gökalp.</code>
+                        <code>Welcome to my personal web page. ^100 Let me introduce myself.</code>
+                        <code>I'am a web master that aged <?=(new DateTime())->diff(new DateTime('1998-09-08'))->format('%y')?> and who is dedicated himself to programming.</code>
+                        <code>I started programming when i was a little child with desktop applications. After then i've continued developing with web applications.</code>
+                        <code>Currently i'm developing high level backend applications with Symfony php framework, Nodejs with /(no)?sql/. I also used Golang, C#, C++, Lua, Phyton, ActionScript, Asp.NET ... and more of them before.</code>
+                        <code>I'm also interested in frontend applications that using React, VueJs with ES6 <i class="fa fa-heart" style="color: red"></i> and of course i've knowledge of task Runners like grunt, gulp.pipe(angular).pipe(lodash).pipe(LESS).pipe(SASS) etc.</code>
+                        <code>You can contact with me using your mobile phone <a href="tel:05375776564" itemprop="telephone">0(537) 577-65-64</a> or e-mail: <a href="mailto:iletisim@mehmetgokalp.com" itemprop="email">iletisim@mehmetgokalp.com</a>.</code>
+                        <code>See you then ;), have a good day!</code>
+                    </p>
                 </div>
                 <span id="message" style="white-space: pre-line;"></span>
+                <div class="help">Use <button type="button" class="btn escBtn">ESC</button> key to skip animation</div>
             </article>
         </section>
 
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/typed.min.js"></script>
         <script>
-            $(document).on('ready', function(){
-                var anim = false,
-                    isTyping = true;
+            var isStopped = false,
+                typedInstance;
+            
+            function stopAnimate() {
+                if (isStopped === false) {
+                    typedInstance.destroy();
+                    $('#messages').addClass('stopAnimate').show(0);
+                    
+                    isStopped = true;
+                }
+            }
+            
+            function bindESCEvents() {
+                $(document).keyup(function(e) {
+                    if (e.keyCode === 27) { // escape key maps to keycode `27`
+                        stopAnimate();
+                    }
+                }).on('click', '.escBtn', function () {
+                    stopAnimate();
+                });
+            }
+            
+            function constructTyping() {
+                var anim = false;
 
-                $('#message').typed({
-                    stringsElement: $('#messages'),
-                    typeSpeed: 30,
-                    backDelay: 500,
+                typedInstance = new Typed('#message', {
+                    stringsElement: '#messages',
+                    typeSpeed: 10,
                     loop: false,
                     contentType: 'html',
-                    loopCount: false,
-                    callback: function(){
-                        isTyping = false;
-                    }
+                    loopCount: false
                 });
 
+                var $terminalBody = $('.terminal-body');
                 setInterval(function(){
-                    if(!anim && isTyping){
+                    if(!anim){
                         anim = true;
-                        $('.terminal-body').animate({ scrollTop: $('.terminal-body').prop("scrollHeight")}, 100, function(){
+
+                        $terminalBody.animate({ scrollTop: $terminalBody.prop("scrollHeight")}, 100, function(){
                             anim = false;
                         });
                     }
                 }, 100);
+                
+                bindESCEvents();
+            }
+            
+            function destructTyping() {
+            
+            }
+            
+            $(document).on('ready', function(){
+                constructTyping();
             });
         </script>
         <script>
